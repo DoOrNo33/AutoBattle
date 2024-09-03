59d0de09b151285bae9f8fa9180af2d21d2080a4
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-09-03 19:15:44 +0900

    [Feature] 적 정보

--- Assets/01_Scripts/Enemy/Enemy.cs
+++ Assets/01_Scripts/Enemy/Enemy.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("몬스터 정보")]
     // TODO : 몬스터 데이터 CSV에서 가져오기
+    [SerializeField] private string enemyName;
+    [SerializeField] private string enemyGrade;
     [SerializeField] private float baseHealth;
     private float currentHealth;
-
     [SerializeField] private float moveSpeed;
 
     // TODO : 이벤트 구독 방식으로 변경
@@ -41,4 +43,24 @@ public class Enemy : MonoBehaviour
 
         healthUI.UpdateHealthUI(baseHealth, currentHealth);
     }
+
+    //TODO : CSV를 이용해 한번에 세팅하면 될 듯
+    public EnemyInfo GetEnemyInfo()
+    {
+        EnemyInfo info = new EnemyInfo();
+        info.Name = enemyName;
+        info.Grade = enemyGrade;
+        info.Speed = moveSpeed;
+        info.Health = baseHealth;
+
+        return info;
+    }
+}
+
+public struct EnemyInfo
+{
+    public string Name;
+    public string Grade;
+    public float Speed;
+    public float Health;
 }
