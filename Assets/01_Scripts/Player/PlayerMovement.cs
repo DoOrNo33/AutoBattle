3b796724bb1dac23d276d0461c852fbb66381601
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-09-04 10:16:10 +0900

    [Feature] 플레이어 이동

--- Assets/01_Scripts/Player/PlayerMovement.cs
+++ Assets/01_Scripts/Player/PlayerMovement.cs
@@ -1,20 +1,80 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.LowLevel;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Player player;
     [SerializeField] private float distanceThreshold;
+    [SerializeField] private PlayerInputManager inputManager;
+
+    private InputAction move;
+    private Vector2 moveDirection;
+
+    private float leftWallPos = -12;
+    private float rightWallPos = 13;
+
+    //private InputAction mouseLeftClick;
+
+    void Start()
+    {
+        move = inputManager.Move;
+
+        move.performed += OnMove;
+        move.canceled += OnMoveCanceled;
+    }
 
     private void FixedUpdate()
     {
         MovePlayer();
+        FaceEnemy();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveDirection = Vector2.zero;
     }
 
     private void MovePlayer()
+    {
+        if (moveDirection.x != 0)
+        {
+            // 오른쪽 포지션 최대
+            if (player.transform.position.x > rightWallPos)
+            {
+                // Direction이 음수일 때만 이동
+                if (moveDirection.x <= 0)
+                {
+                    player.transform.position += new Vector3(moveDirection.x, 0, 0) * player.playerMoveSpeed * Time.deltaTime;
+
+                }
+            }
+            // 왼쪽 포지션 최대
+            else if (player.transform.position.x < leftWallPos)
+            {
+                // Direction이 양수일 때만 이동
+                if (moveDirection.x >= 0)
+                {
+                    player.transform.position += new Vector3(moveDirection.x, 0, 0) * player.playerMoveSpeed * Time.deltaTime;
+                }
+            }
+            else
+            {
+                player.transform.position += new Vector3(moveDirection.x, 0, 0) * player.playerMoveSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    public void FaceEnemy()
     {
         if (player.PlayerPos == null) return;
         if (player.EnemyPos == null) return;
@@ -33,14 +93,7 @@ public class PlayerMovement : MonoBehaviour
             Vector2 scale = player.transform.localScale;
             scale.x = Mathf.Abs(scale.x);
             player.transform.localScale = scale;
-
-            // 플레이어와 적의 거리가 너무 멀다면 다가감
-            if (distance > distanceThreshold)
-            {
-                //player.transform.position += Vector3.right * player.playerMoveSpeed * Time.deltaTime;
-            }
         }
-
         // 플레이어가 오른쪽이라면
         else
         {
